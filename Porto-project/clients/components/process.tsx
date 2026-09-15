38cff6fd8f46448828ee79b9527a3b021e356d16
RivaldoJR <113871228+Grvld27@users.noreply.github.com>
2026-09-15 17:52:13 +0800

    icon belum ditambahkan

---
+++ Porto-project/clients/components/process.tsx
@@ -0,0 +1,193 @@
+
+
+
+export default function Process () {
+return(
+         <div className="grid-cols-1">
+<div className=" flex items-center justify-center gap-5 xl:mt-50 xl:mb-[-15%]  text-sm ">
+
+         <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000] ">
+<div className=" grid grid-cols-2 xl:mb-3 ">
+<img  className="xl:ml-10 xl:mt-5" width="50" height="50" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+<h1 className=" text-black font-bold xl:mt-6"> Research & Planning </h1>
+</div>
+<p className=" xl:ml-6 w-55 text-black font-semibold">Before diving into development, i make sure really get what the project's about the goals, what users actually need, and all the requirements</p>
+         </div>
+
+
+
+          <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className=" grid grid-cols-2 xl:mb-3  ">
+<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+
+<h1 className=" text-black font-bold xl:mt-6 w-100">Build & Develop</h1>
+</div>
+<p className=" xl:ml-6 w-55 text-black font-semibold"> I bring the design to life with clean, responsive code that scales well, using modern tools HTML, CSS and Javascript framework</p>
+
+         </div>
+
+         
+
+
+       <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className=" grid grid-cols-2 xl:mb-5">
+<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+
+<div className="grid grid-cols-2 xl:mb-5">
+<h1 className=" text-black font-bold xl:mt-6 w-100">Launch</h1>
+
+</div>
+<p className=" xl:ml-6 w-55 text-black font-semibold"> Get the build ready and ship it to production, so it's live and ready for real users</p>
+</div>
+         </div>
+
+
+
+
+
+
+ {/*  */}
+         </div>
+
+<div className="flex items-center justify-center xl:mb-[-15%] ">
+         
+      <svg
+        className="w-full h-full"
+        viewBox="0 0 1200 500"
+        preserveAspectRatio="none"
+      >
+        {/* Garis horizontal utama */}
+        <path
+          d="M 380 250 H 870"
+          stroke="black"
+          strokeWidth="4"
+          fill="none"
+          stroke-linecap="round"
+        />
+
+        {/* Connector atas kiri */}
+        <path
+          d="M 380 200 V 250"
+          stroke="black"
+          strokeWidth="4"
+          fill="none"
+           stroke-linecap="round"
+        />
+
+        {/* Connector atas tengah */}
+        <path
+          d="M 600 200 V 250"
+          stroke="black"
+          strokeWidth="4"
+          fill="none"
+           stroke-linecap="round"
+        />
+
+        {/* Connector atas kanan */}
+        <path
+          d="M 820 200 V 250"
+          stroke="black"
+          strokeWidth="4"
+          fill="none"
+                  stroke-linecap="round"
+        />
+
+        {/* Connector bawah kiri */}
+        <path
+          d="M 435 300 V 250"
+          stroke="black"
+          strokeWidth="4"
+          fill="none"
+          stroke-linecap="round"
+        />
+
+        {/* Connector bawah tengah */}
+        <path
+          d="M 650 300 V 250"
+          stroke="black"
+          strokeWidth="4"
+          fill="none"
+                  stroke-linecap="round"
+
+        />
+
+        {/* Connector bawah kanan */}
+        <path
+          d="M 870 300 V 250"
+          stroke="black"
+          strokeWidth="4"
+          fill="none"
+                  stroke-linecap="round"
+        />
+
+        {/* Titik atas */}
+        <circle cx="380" cy="195" r="6" fill="black" />
+        <circle cx="600" cy="195" r="6" fill="black" />
+        <circle cx="820" cy="195" r="6" fill="black" />
+
+        {/* Titik bawah */}
+        <circle cx="435" cy="306" r="6" fill="black" />
+        <circle cx="650" cy="306" r="6" fill="black" />
+        <circle cx="870" cy="306" r="6" fill="black" />
+      </svg>
+  
+</div>
+
+
+
+
+<div className=" flex items-center justify-center gap-5 xl:ml-30    text-sm ">
+
+          <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000] ">
+<div className=" grid grid-cols-2 xl:mb-3 ">
+<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+<h1 className=" text-black font-bold xl:mt-6"> UI/UX Design </h1>
+</div>
+<p className=" xl:ml-6 w-55 text-black font-semibold">Sketch out wireframes and design visuals in Figma, keeping things clean, simple, and se</p>
+         </div>
+
+
+
+
+
+
+
+
+          <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className=" grid grid-cols-2 xl:mb-3">
+<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+
+
+<h1 className=" text-black font-bold xl:mt-6 w-100">Testing</h1>
+
+</div>
+<p className=" xl:ml-6 w-55 text-black font-semibold"> Step four. I'm testing how it works, how it loos, and how it feels across devices, then polishing until it's just right</p>
+
+         </div>
+
+         
+
+
+       <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className=" grid grid-cols-2 xl:mb-3">
+<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+
+
+<h1 className=" text-black font-bold xl:mt-6 w-100">Maintenance</h1>
+
+</div>
+<p className=" xl:ml-6 w-55 text-black font-semibold">And it doesn't stop there. I keep improving things with updates, bug fixes, performance tweaks, and new features based on what users say</p>
+
+         </div>
+
+ 
+         </div>
+
+
+
+
+
+</div>
+)
+
+}
