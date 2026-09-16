463b50a0d519bed5087798b1f54e12aaac1417c8
RivaldoJR <113871228+Grvld27@users.noreply.github.com>
2026-09-16 22:51:58 +0800

    update icon dan hover

---
+++ Porto-project/clients/components/services.tsx
@@ -0,0 +1,65 @@
+export default function Service () {
+
+
+return (
+
+         <div className=" flex flex-col items-center justify-center  xl:mt-40">
+
+<h1 className="  text-3xl flex items-center justify-center    text-m w-80 h-7 gap-1 text-black font-semibold">
+         Services I Offer
+</h1>
+                  
+<div className="grid grid-cols-3 gap-10 xl:mt-30">
+
+ <div className="group transition-all duration-200 hover:scale-105 bg-white w-65 h-90 rounded-2xl shadow-[0_3px_9px_-6px_#000000] ">
+<div className=" relative grid grid-cols-1 xl:mb-3  ">
+<img  className="  xl:ml-25 xl:mt-5 group-hover:hidden" width="48" height="48" src="https://img.icons8.com/ios/50/laptop-coding.png" alt="what-i-do"/>
+<img  className="  xl:ml-25 xl:mt-5  hidden group-hover:block" width="48" height="48" src="https://img.icons8.com/ios-filled/50/laptop-coding.png" alt="what-i-do"/>
+
+
+
+
+<h1 className="flex items-center justify-center text-black font-bold xl:mt-6"> Web Development </h1>
+</div>
+<p className=" flex items-center justify-center xl:ml-6 xl:mt-10 text-black font-semibold">Building smooth, responsive, and scalable websites with modern tools like HTML, CSS and Javascript framework</p>
+         </div>
+
+
+          <div className=" group transition-all duration-200 hover:scale-105 bg-white w-65 h-90 rounded-2xl shadow-[0_3px_9px_-6px_#000000] ">
+<div className=" grid grid-cols-1 xl:mb-3  ">
+<img  className=" xl:ml-25 xl:mt-5 group-hover:hidden" width="48" height="48" src="https://img.icons8.com/ios/50/windows10-personalization.png" alt="what-i-do"/>
+<img  className=" xl:ml-25 xl:mt-5 hidden group-hover:block" width="48" height="48" src="https://img.icons8.com/ios-filled/50/windows10-personalization.png" alt="what-i-do"/>
+
+<h1 className="flex items-center justify-center text-black font-bold xl:mt-6"> UI/UX Design </h1>
+</div>
+<p className=" flex items-center justify-center xl:ml-6 xl:mt-10 text-black font-semibold">Crafting interfaces that are clean, intuitive, and easy to use with usability, consistency, and a seamless experience in mind</p>
+         </div>
+
+
+ <div className="group transition-all duration-200 hover:scale-105 bg-white w-65 h-90 rounded-2xl shadow-[0_3px_9px_-6px_#000000] ">
+<div className=" relative grid grid-cols-1 xl:mb-3  ">
+<img  className=" xl:ml-25 xl:mt-5 group-hover:hidden" width="48" height="48" src="https://img.icons8.com/ios/50/smartphone--v2.png" alt="what-i-do"/>
+
+<img  className=" xl:ml-25 xl:mt-5 hidden group-hover:block" width="48" height="48" src="https://img.icons8.com/ios-filled/50/smartphone--v2.png" alt="what-i-do"/>
+
+<h1 className="flex items-center justify-center text-black font-bold xl:mt-6"> Mobile App  </h1>
+</div>
+<p className=" flex items-center justify-center xl:ml-6 xl:mt-10 text-black font-semibold">Designing and building mobile apps that are responsive, look modern, and feels smooth across different</p>
+         </div>
+
+</div>
+
+
+
+         
+</div>
+
+
+
+);
+
+
+
+
+
+}
