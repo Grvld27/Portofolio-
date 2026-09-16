400126fa6f02cc56ec67b616dba1acd13e21f61b
RivaldoJR <113871228+Grvld27@users.noreply.github.com>
2026-09-16 22:51:28 +0800

    tambahin icon baru dan hover

--- Porto-project/clients/components/process.tsx
+++ Porto-project/clients/components/process.tsx
@@ -1,45 +1,174 @@
+import { nav } from "framer-motion/client";
+
+import plan1 from "https://img.icons8.com/ios/50/test--v1.png";
+
+
+
+
+
+
+
+
+const navItem =[
+
+
+  
+{ icon:(
+
+<img src= 'https://img.icons8.com/ios/50/test--v1.png'  width="48" height="48"  alt="what-i-do"/>
+
+),
+iconHoover:(<img src='https://img.icons8.com/ios-filled/50/test.png'   width="48" height="48"  alt="what-i-do"/>),
+
+
+icon1:(
+<img src ='https://img.icons8.com/ios/50/code--v2.png' width='48' height='48'/>
+  
+),
+iconhoover1:(<img src='https://img.icons8.com/ios-filled/50/code.png' width='48' height='48'/>),
+
+icon2:(
+<img src='https://img.icons8.com/ios/50/launched-rocket.png' width='48' height='48'/>
+),
+iconhoover2:(
+
+  <img src='https://img.icons8.com/ios-filled/50/launched-rocket.png' width='48' height='48'/>
+),
+
+
+icon3:(
+<img src='https://img.icons8.com/ios/50/design--v1.png' width='48' height='48'/>
+),
+iconhoover3:(
+
+  <img src='https://img.icons8.com/ios-filled/50/design--v1.png' width='48' height='48'/>
+),
+
+icon4:(
+<img src='https://img.icons8.com/ios/50/test-results.png' width='48' height='48'/>
+),
+iconhoover4:(
+
+  <img src='https://img.icons8.com/ios-filled/50/test-results.png' width='48' height='48'/>
+),
+
+
+icon5:(
+<img src='https://img.icons8.com/ios/50/service--v1.png' width='48' height='48'/>
+),
+iconhoover5:(
+
+  <img src='https://img.icons8.com/ios-filled/50/service.png' width='48' height='48'/>
+),
+  
+
+
+}
+
+
+
+]
+
+
+
+
+
 
 
 
 export default function Process () {
 return(
          <div className="grid-cols-1">
-<div className=" flex items-center justify-center gap-5 xl:mt-50 xl:mb-[-15%]  text-sm ">
+      
+          
+     
 
-         <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000] ">
-<div className=" grid grid-cols-2 xl:mb-3 ">
-<img  className="xl:ml-10 xl:mt-5" width="50" height="50" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
-<h1 className=" text-black font-bold xl:mt-6"> Research & Planning </h1>
+
+
+ {navItem.map((item,index) => (
+
+
+<div className=" flex items-center justify-center  gap-5 xl:mt-50 xl:mb-[-15%]  text-sm ">
+
+<div 
+key={index}
+className=" group hover:bg-white   transition-all
+              duration-200
+              hover:scale-105
+    
+ w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className=" relative grid grid-cols-2 mt-[-5%]">
+
+
+
+<div className=" group-hover:hidden xl:ml-10 xl:mt-5 ">
+{item.icon}
 </div>
-<p className=" xl:ml-6 w-55 text-black font-semibold">Before diving into development, i make sure really get what the project's about the goals, what users actually need, and all the requirements</p>
+<div className=" hidden group-hover:block  xl:ml-10 xl:mt-5">
+{item.iconHoover}
+</div>
+
+
+<h1 className=" text-black font-bold xl:mt-6 xl:mr-10 group-hover:block"> Research & Planning </h1>
+</div>
+<p className=" xl:ml-6 xl:mt-5 w-55 text-justify  text-black font-semibold">Before diving into development, i make sure really get what the project's about the goals, what users actually need, and all the requirements</p>
          </div>
 
 
 
-          <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
-<div className=" grid grid-cols-2 xl:mb-3  ">
-<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+          <div
+          key={index}
+          className=" group hover:bg-white   transition-all
+              duration-200
+              hover:scale-105 w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className=" relative grid grid-cols-2 xl:mb-3  ">
 
-<h1 className=" text-black font-bold xl:mt-6 w-100">Build & Develop</h1>
+<div className=" group-hover:hidden xl:ml-10 xl:mt-3 ">
+{item.icon1}
 </div>
-<p className=" xl:ml-6 w-55 text-black font-semibold"> I bring the design to life with clean, responsive code that scales well, using modern tools HTML, CSS and Javascript framework</p>
+<div className=" hidden group-hover:block  xl:ml-10 xl:mt-3  ">
+{item.iconhoover1}
+</div>
+
+
+
+<h1 className=" text-black font-bold xl:mt-6 w-100 group-hover:block">Build & Develop</h1>
+</div>
+<p className=" xl:ml-4 w-58 text-justify text-black font-semibold"> I bring the design to life with clean, responsive code that scales well, using modern tools HTML, CSS and Javascript framework</p>
 
          </div>
 
          
 
 
-       <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
-<div className=" grid grid-cols-2 xl:mb-5">
-<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+       <div className=" group transition-all
+              duration-200
+              hover:scale-105 bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className="  relative grid grid-cols-2 xl:mb-5">
+<div className=" group-hover:hidden xl:ml-10 xl:mt-3 ">
+{item.icon2}
+</div>
+<div className=" hidden group-hover:block  xl:ml-10 xl:mt-3  ">
+{item.iconhoover2}
+</div>
 
 <div className="grid grid-cols-2 xl:mb-5">
 <h1 className=" text-black font-bold xl:mt-6 w-100">Launch</h1>
 
 </div>
-<p className=" xl:ml-6 w-55 text-black font-semibold"> Get the build ready and ship it to production, so it's live and ready for real users</p>
+<p className=" xl:ml-6 xl:mt-2 w-55 text-black font-semibold"> Get the build ready and ship it to production, so it's live and ready for real users</p>
 </div>
          </div>
+
+
+      </div>
+
+ 
+ 
+    ))}
+
+
+         
 
 
 
@@ -47,7 +176,7 @@ return(
 
 
  {/*  */}
-         </div>
+   
 
 <div className="flex items-center justify-center xl:mb-[-15%] ">
          
@@ -136,26 +265,40 @@ return(
 
 
 
-<div className=" flex items-center justify-center gap-5 xl:ml-30    text-sm ">
 
-          <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000] ">
-<div className=" grid grid-cols-2 xl:mb-3 ">
-<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+
+ {navItem.map((item,index) => (
+<div
+  key={index}
+  className=" flex items-center justify-center gap-5 xl:ml-30    text-sm ">
+
+
+  <div className=" group transition-all duration-200 hover:scale-105 bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000] ">
+<div className=" relative grid grid-cols-2 xl:mb-3 ">
+
+<div className=" group-hover:hidden xl:ml-10 xl:mt-3 ">
+{item.icon3}
+</div>
+<div className=" hidden group-hover:block  xl:ml-10 xl:mt-3  ">
+{item.iconhoover3}
+</div>
+
+
 <h1 className=" text-black font-bold xl:mt-6"> UI/UX Design </h1>
 </div>
-<p className=" xl:ml-6 w-55 text-black font-semibold">Sketch out wireframes and design visuals in Figma, keeping things clean, simple, and se</p>
+<p className=" xl:ml-6 w-55 text-start text-black font-semibold">Sketch out wireframes and design visuals in Figma, keeping things clean, simple, and se</p>
          </div>
 
+<div className=" group transition-all hover:scale-105 bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className=" relative grid grid-cols-2 xl:mb-3">
 
+<div className=" group-hover:hidden xl:ml-10 xl:mt-3 ">
+{item.icon4}
+</div>
+<div className=" hidden group-hover:block  xl:ml-10 xl:mt-3  ">
+{item.iconhoover4}
+</div>
 
-
-
-
-
-
-          <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
-<div className=" grid grid-cols-2 xl:mb-3">
-<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
 
 
 <h1 className=" text-black font-bold xl:mt-6 w-100">Testing</h1>
@@ -168,10 +311,15 @@ return(
          
 
 
-       <div className="bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
-<div className=" grid grid-cols-2 xl:mb-3">
-<img  className="xl:ml-4 xl:mt-5" width="48" height="48" src="https://img.icons8.com/fluency-systems-filled/48/what-i-do.png" alt="what-i-do"/>
+       <div className="group transition-all hover:scale-105 bg-white w-65 h-50 rounded-2xl shadow-[0_3px_9px_-6px_#000000]  ">
+<div className=" relative grid grid-cols-2 xl:mb-3">
 
+<div className=" group-hover:hidden xl:ml-10 xl:mt-3 ">
+{item.icon5}
+</div>
+<div className=" hidden group-hover:block  xl:ml-10 xl:mt-3  ">
+{item.iconhoover5}
+</div>
 
 <h1 className=" text-black font-bold xl:mt-6 w-100">Maintenance</h1>
 
@@ -187,7 +335,30 @@ return(
 
 
 
+
+
+
+
+)
+)} 
+
+        
+
+
+
+
+
+
+
+
+          
+
+
+    
+
 </div>
+
+    
 )
 
 }
