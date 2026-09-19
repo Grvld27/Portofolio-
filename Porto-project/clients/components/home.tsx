26ad26f5ca7423da32c1514d12b6250727aed879
RivaldoJR <113871228+Grvld27@users.noreply.github.com>
2026-09-19 22:20:06 +0800

    belum bikin api untuk button download cv dan hire me, dan belum bikin url link sosmed

--- Porto-project/clients/components/home.tsx
+++ Porto-project/clients/components/home.tsx
@@ -1,102 +1,105 @@
-import fotoSaya from '../assets/Jas putih bg remove.png'
-import placeHolder from '../assets/placeHold.png'
-import briefCase from '../assets/briefcase.png'
-import linkedIn from '../assets/linkedin.png'
-import discord from '../assets/discord.png'
-import github from '../assets/github.png'
-import instagram from '../assets/instagram.png'
-
-
+import fotoSaya from "../assets/Jas putih bg remove.png";
+import placeHolder from "../assets/placeHold.png";
+import briefCase from "../assets/briefcase.png";
+import linkedIn from "../assets/linkedin.png";
+import discord from "../assets/discord.png";
+import github from "../assets/github.png";
+import instagram from "../assets/instagram.png";
 
 // const navItem = [
 
 // Icon:''
 
-
-
 // ]
 
+export default function Home() {
+  return (
+    <div className=" text-black font-jakarta m-25 justify-center">
+      <h1 className="bg-gray-200 rounded-full text-m w-80 h-7 gap-1  flex items-center justify-center text-black font-semibold">
+        Available for freelance work
+      </h1>
 
+      <div className="grid grid-cols-2 gap-1 items-center justify-center mt-[-5%] ">
+        <h2 className="text-6xl font-extrabold w-160 mt-[-20%] ">
+          Hi, I'm Rivaldo Jollien Ramdarto
+        </h2>
 
+        <img
+          src={fotoSaya}
+          alt="Foto Saya"
+          className="w-104 h-104 ml-50  object-cover rounded-xl"
+        />
+      </div>
 
-export default function Home (){
-return (
+      {/* Profile Information */}
+      <div className="grid grid-cols-1 mt-[-15%] gap-2">
+        <p className="text-2xl font-semibold w-120 font-inter">
+          Frontend Developer
+        </p>
+        <p className="w-120 text-sm">
+          By day, I'm just a regular dev by night (okay, also by day), I'm
+          swinging between frontend code and UI/UX design, web slinging ideas
+          into web apps that actually work and look awesome.
+        </p>
+      </div>
 
-<div className=" text-black font-jakarta m-25 justify-center">
+      {/* Location and Availability */}
+      <div className="grid grid-cols-2  mt-6">
+        <div className="grid grid-cols-2   ">
+          <img
+            src={placeHolder}
+            alt="Placeholder"
+            className="w-6 h-6  ml-5  "
+          />
+          <p className="md:ml-[-65%] xl:ml-[-80%]">Kutai Barat</p>
+        </div>
 
+        <div className="grid grid-cols-2 gap-0 ml-[-80%] ">
+          <img src={briefCase} alt="Briefcase" className="w-6 h-6  ml-20  " />
+          <p className="md:ml-[-55%] xl:ml-[-76%]">Available now</p>
+        </div>
+      </div>
 
-<h1 className="bg-gray-200 rounded-full text-m w-80 h-7 gap-1  flex items-center justify-center text-black font-semibold">
-         Available for freelance work
-</h1>
+      <div className="grid grid-cols-2  mt-10">
+        <div className="grid grid-cols-2 ">
+          <button className="bg-black text-white rounded-full w-40 h-10 font-semibold hover:bg-gray-800 transition duration-300">
+            Hire Me
+          </button>
+          <button className="bg-white text-black rounded-full w-40 h-10 font-semibold border md:ml-[-2%] xl:ml-[-45%] border-black hover:bg-gray-200 transition duration-300">
+            Download CV
+          </button>
 
+          {/*  */}
+          <p className=" xl:mt-[-10%] w-100 ml-90 ">Follow me</p>
 
+          <div className="flex space-x-4 xl:ml-[50%] xl:mt-[-11%] gap-3">
+            <img
+              src={linkedIn}
+              alt="LinkedIn"
+              className="w-8 h-8 cursor-pointer hover:opacity-70"
+            />
+            <img
+              src={discord}
+              alt="Discord"
+              className="w-8 h-8 cursor-pointer hover:opacity-70"
+            />
+            <img
+              src={github}
+              alt="GitHub"
+              className="w-8 h-8 cursor-pointer hover:opacity-70"
+            />
+            <img
+              src={instagram}
+              alt="Instagram"
+              className="w-8 h-8 cursor-pointer hover:opacity-70"
+            />
+          </div>
+          {/*  */}
 
-
-<div className="grid grid-cols-2 gap-1 items-center justify-center mt-[-5%] ">
-         <h2 className="text-6xl font-extrabold w-160 mt-[-20%] ">Hi, I'm Rivaldo Jollien Ramdarto</h2>
-
-<img src={fotoSaya} alt="Foto Saya" className="w-104 h-104 ml-50  object-cover rounded-xl" />
-
-</div>
-  
-
-{/* Profile Information */}
-<div className='grid grid-cols-1 mt-[-15%] gap-2'>
-<p className="text-2xl font-semibold w-120 font-inter">Frontend Developer</p>
- <p className="w-120 text-sm">By day, I'm just a regular dev  by night (okay, also by day), I'm swinging between frontend code and UI/UX design, web slinging ideas into web apps that actually work and look awesome.</p>  
-  </div>
-
-{/* Location and Availability */}
-<div className='grid grid-cols-2  mt-6'>
-
-  <div className='grid grid-cols-2   '>
-<img src={placeHolder} alt="Placeholder" className="w-6 h-6  ml-5  " />
-<p className="md:ml-[-65%] xl:ml-[-80%]">Kutai Barat</p>
-</div>
-
-  <div className='grid grid-cols-2 gap-0 ml-[-80%] '>
-<img src={briefCase} alt="Briefcase" className="w-6 h-6  ml-20  " />
-<p className="md:ml-[-55%] xl:ml-[-76%]">Available now</p>
-</div>
-</div>
-
-
-
-<div className="grid grid-cols-2  mt-10">
-
-<div className="grid grid-cols-2 ">
-         <button className="bg-black text-white rounded-full w-40 h-10 font-semibold hover:bg-gray-800 transition duration-300">
-          Hire Me
-         </button>
-         <button className="bg-white text-black rounded-full w-40 h-10 font-semibold border md:ml-[-2%] xl:ml-[-45%] border-black hover:bg-gray-200 transition duration-300">
-          Download CV
-         </button>
-
-
-
-  {/*  */}
-         <p className=" xl:mt-[-10%] w-100 ml-90 ">
-Follow me
-         </p>
-
-         <div className="flex space-x-4 xl:ml-[50%] xl:mt-[-11%] gap-3">
-           <img src={linkedIn} alt="LinkedIn" className="w-8 h-8 cursor-pointer hover:opacity-70" />
-           <img src={discord} alt="Discord" className="w-8 h-8 cursor-pointer hover:opacity-70" />
-           <img src={github} alt="GitHub" className="w-8 h-8 cursor-pointer hover:opacity-70" />
-           <img src={instagram} alt="Instagram" className="w-8 h-8 cursor-pointer hover:opacity-70" />
-         </div>
-{/*  */}
-
-
-<div className="justify-center items-center mt-5 bg-black rounded-xl xl:w-330 h-20 gap-1  flex text-white font-semibold">
-
-
-<svg
-viewBox="0 0 1800 300"
-      className="w-250 xl:mt-5 h-auto"
-    >
-     <path
-    d="
+          <div className="justify-center items-center mt-5 bg-black rounded-xl xl:w-330 h-20 gap-1  flex text-white font-semibold">
+            <svg viewBox="0 0 1800 300" className="w-250 xl:mt-5 h-auto">
+              <path
+                d="
       M -20 220
 
       C 80 110, 170 70, 250 95
@@ -123,38 +126,16 @@ viewBox="0 0 1800 300"
       C 1410 100, 1430 210, 1490 180
       C 1550 150, 1580 100, 1600 50
     "
-    stroke="white"
-    strokeWidth="8"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    fill="none"
-  />
-    </svg>
-  
-
-
-</div>
-
-
-
-
-    
-
-
-</div>
-
-
-</div>
-
-
-
-</div>
-
-
-
-
-);
-
-
-
+                stroke="white"
+                strokeWidth="8"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
